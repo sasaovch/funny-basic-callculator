7a3ab9ef7c76123cf9234c804dcf81dbaecf901a
Atella <UpolushkinM@mail.ru>
2022-09-22 18:20:22 +0300

    invalid base fix

--- src/change_base.c
+++ src/change_base.c
@@ -36,7 +36,7 @@ enum change_base_result convert_to_base_n_string(unsigned long val, int radix, c
 
 enum change_base_result convert_string_to_base_n(const char *str, int radix, unsigned long *val) {
     // if radix is not supported
-    if (radix > 16 || radix < 2) { return false; }
+    if (radix > 16 || radix < 2) { return INVALID_BASE; }
 
     static const int char_to_int_map[] = {['0'] = 0, 1, 2,          3,  4,  5,  6,  7,
                                           8,         9, ['a'] = 10, 11, 12, 13, 14, 15};
@@ -50,5 +50,5 @@ enum change_base_result convert_string_to_base_n(const char *str, int radix, uns
         if (prev > *val) { return OVERFLOW; }
     }
 
-    return true;
+    return OK;
 }
